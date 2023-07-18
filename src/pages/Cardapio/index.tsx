0570017254d59e46f8f0907eeb90116bc51c254c
Tiago Oliveira <titiago2504@gmail.com>
2023-07-18 09:46:53 -0300

    Create: Insert Filter in menu search

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo}  from 'assets/logo.svg';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
 
@@ -25,6 +26,9 @@ export default function Cardapio() {
                 busca={busca} 
                 setBusca={setBusca}
                 />
+                <div className={styles.cardapio__filtros}>
+                  <Filtros />
+                </div>
             </section>
         </main>
     )
